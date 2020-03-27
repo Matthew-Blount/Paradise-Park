dde4906c2a364d989a87f0065656721a6d92ea4d
santiagomb17 <60156047+santiagomb17@users.noreply.github.com>
2020-03-27 14:55:58 -0500

    temp code that coincides with levels

---
+++ Paradise Park/Assets/code/targetmove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetmove : MonoBehaviour
+{
+    //this script is attatched to columns that either move themselves or objects when hit at a certain point by an enemy
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Paradise Park/Assets/code/tempMovingBlock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempMovingBlock : MonoBehaviour
+{
+    private Transform movingPlatform;
+    public Transform position1;
+    public Transform position2;
+    private Vector2 newPostion;
+    //public string currentState;
+    private float speed;
+    //private float resetTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        //ChangeTarget();
+        //resetTime = 5;
+        speed = (float)1.5;
+        movingPlatform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Mathf.Abs(movingPlatform.position.x - position1.position.x) > .01)
+        {
+            movingPlatform.position = Vector2.Lerp(movingPlatform.position, position1.position, speed * Time.deltaTime);
+        }           
+        else if (Mathf.Abs(movingPlatform.position.x - position2.position.x) > .01)
+        {
+            movingPlatform.position = Vector2.Lerp(movingPlatform.position, position2.position, speed * Time.deltaTime);
+        }
+        else
+        {                //position 1 diff                                           //position 2 diff
+            if(Mathf.Abs(movingPlatform.position.x - position1.position.x) < Mathf.Abs(movingPlatform.position.x - position2.position.x) )
+                movingPlatform.position = Vector2.Lerp(movingPlatform.position, position1.position, speed * Time.deltaTime);
+
+        }
+    }
+
+}
